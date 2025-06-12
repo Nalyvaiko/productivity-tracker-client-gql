3ac1c3ead03c85f25e44f8c2f502e40d15ad98f2
Vitalii <v.yu.nalyvaiko@gmail.com>
2025-06-11 20:37:14 -0700

    Clean up

--- app/api/graphql/resolvers.ts
+++ app/api/graphql/resolvers.ts
@@ -1,7 +1,7 @@
 import { db } from '@/db/db'
 import { InsertIssues, SelectIssues, issues, users } from '@/db/schema'
 import { GQLContext } from '@/types'
-import { getUserFromToken, signin, signup } from '@/utils/auth'
+import { signin, signup } from '@/utils/auth'
 import { and, asc, desc, eq, or, sql } from 'drizzle-orm'
 import { GraphQLError } from 'graphql'
 
